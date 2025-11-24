92d91bfb5d660d0398eaea292d4c641d8aad2941
Ilya <creatoreli8@gmail.com>
2025-11-24 14:29:32 +0300

    improved main page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 // import mkcert from 'vite-plugin-mkcert'
 // import fs from 'fs';
 // import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -23,6 +24,6 @@ export default defineConfig({
     port: 3001
   },
   //plugins: [react(), mkcert(),],
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   base: '/times_and_armies_front/'
 })
